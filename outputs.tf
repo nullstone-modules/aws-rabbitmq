d8f6795dc9e3c4d9a0b23400d650d2d07b034452
Brad Sickles <brad.sickles@gmail.com>
2022-05-18 17:06:17 -0400

    Added security group

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "db_admin_secret_name" {
   value       = aws_secretsmanager_secret.admin.name
   description = "string ||| The name of the secret in AWS Secrets Manager containing the admin credentials"
 }
+
+output "db_security_group_id" {
+  value       = aws_security_group.this.id
+  description = "string ||| The ID of the security group attached to the RabbitMQ instance."
+}
